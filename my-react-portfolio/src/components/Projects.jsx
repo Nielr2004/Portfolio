ed3ea5e4889a5fe6955573c13e31d76cfd6e12bb
Nielr2004 <roysnehashis2004@gmail.com>
2025-07-07 17:39:35 +0530

    Fix

--- my-react-portfolio/src/components/Projects.jsx
+++ my-react-portfolio/src/components/Projects.jsx
@@ -12,11 +12,13 @@ const Projects = () => {
           <p>Smart attendance using OpenCV, Tkinter and Arduino</p>
         </div>
         </a>
+        <a href="https://github.com/Nielr2004/Portfolio.git" target="_blank" rel="noopener noreferrer" className="card-link">
         <div className="card">
           <i className="fas fa-laptop-code fa-2x"></i>
           <h3>This Portfolio</h3>
           <p>One-page Flask site with smooth scroll</p>
         </div>
+        </a>
         <div className="card">
           <i className="fas fa-chart-bar fa-2x"></i> {/* Added icon for data visualization */}
           <h3>Netflix Data Visualization</h3>
@@ -27,6 +29,13 @@ const Projects = () => {
           <i className="fas fa-hand-paper fa-2x"></i> {/* Added icon for hand tracking */}
           <h3>Hands-on volume rocker</h3>
           <p>Python script for a finger tracking volume rocker</p>
+        </div>
+        </a>
+        <a href="https://github.com/Nielr2004/Movie-review-website.git" target="_blank" rel="noopener noreferrer" className="card-link">
+        <div className="card">
+          <i className="fas fa-hand-paper fa-2x"></i> {/* Added icon for hand tracking */}
+          <h3>ViewVerse</h3>
+          <p>A wovie/weries review and watchlist website (in progress)</p>
         </div>
         </a>
       </div>
